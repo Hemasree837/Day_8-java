a68dcd55e42445c471f7e5f8362f39f7d612f423
Hemasree837 <hemasree837@gmail.com>
2024-12-09 15:13:26 +0530

    Add files via upload

---
+++ Sample2D.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Sample2D {
+    public static void main(String args[])
+    {
+        int a[][]={{4,6},{7,9}};
+        System.out.println(Arrays.toString(a[0]));
+        System.out.println(Arrays.toString(a[1]));
+    }
+}
+/*
+ [4, 6]
+[7, 9]
+ */
